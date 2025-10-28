7220fac8826bd5add709e66f98d2b0ebf9f61612
kamsiogbonna631-ux <kamsiogbonna631@gmail.com>
2025-10-28 12:23:52 +0100

    chapter one assignment

---
+++ HelloWorld.java
@@ -0,0 +1,5 @@
+public class HelloWorld{
+	public static void main(Strings[] agrgs) {
+	system.out.println("Hello World, This is my first java program");
+	}
+	
---
+++ OperatorsPart1.java
@@ -0,0 +1,24 @@
+public class OperatorsPart1{
+
+public static void main(string [] args){
+	int num = 50;
+
+num += 8;
+System.out.printf("The value of num is %d%n", num);
+
+num -= 7;
+System.out.printf("The value of num is %d%n", num);
+
+num *= 10;
+System.out.printf("The value of num is %d%n", num);
+
+num /= 5;
+System.out.printf("The value of num is %d%n", num);
+
+num %= 3;
+System.out.printf("The value of num is %d%n", num);
+
+//Arithmetic operatorsperators
+int num1 =10;
+int num2 =4;
+String name
